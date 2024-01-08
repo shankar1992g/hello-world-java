e4069d89e1274e70d2f65168ae5ed1258a70870d
shankar1992g <137378874+shankar1992g@users.noreply.github.com>
2024-01-08 17:45:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,3 +8,13 @@ public class HelloWorld {
     System.out.println("Hello world!");
   }
 }
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
